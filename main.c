63be0f609f5b7acb48938dd5336b5fbdba4e643a
HAKSAN <cranemont@gmail.com>
2021-03-24 19:53:53 +0900

    .

--- main.c
+++ main.c
@@ -19,10 +19,11 @@ int* asleep_pid;
 int timer;
 int processing_time;
 int p_num;
+int running;
 Process* pid;
 
 typedef struct QNodeStruct {
-    Process *p;
+    int q_pid;//Process *p; // int pid?
     struct QNodeStruct *next;
 } QNode;
 
@@ -39,19 +40,23 @@ typedef struct ReadyQueueStruct {
 } RQ;
 RQ *rq;
 
-void QueueInit(Queue *_q, int _time_quantum) {
-    _q = (Queue*)malloc(sizeof(Queue));
+Queue* QueueInit(int _time_quantum) {
+    Queue* _q = (Queue*)malloc(sizeof(Queue));
     _q->front = NULL;
     _q->rear = NULL;
     _q->count = 0;
     _q->time_quantum = _time_quantum;
+    return _q;
 }
 
 void ReadyQueueInit() {
-    QueueInit(rq->process_queue[0], 1);
-    QueueInit(rq->process_queue[1], 2);
-    QueueInit(rq->process_queue[2], 4);
-    QueueInit(rq->process_queue[3], -1);
+    rq->process_queue[0] = QueueInit(1);
+    rq->process_queue[1] = QueueInit(2);
+    rq->process_queue[2] = QueueInit(4);
+    rq->process_queue[3] = QueueInit(-1);
+    // QueueInit(rq->process_queue[1], 2);
+    // QueueInit(rq->process_queue[2], 4);
+    // QueueInit(rq->process_queue[3], -1);
 }
 
 void ReadyQueueFree() {
@@ -78,13 +83,13 @@ void RunProcess(Process *_p) {
 
 }
 
-void QueuePush(int _qnum, Process *_p) {
+void QueuePush(int _qnum, int pid_num) {
     QNode *new_node = (QNode*)malloc(sizeof(QNode));
     new_node->next = NULL;
-    new_node->p = _p;
-    
+    new_node->q_pid = pid_num;
+    // printf("here push\n");
     Queue *q = rq->process_queue[_qnum];
-    printf("111\n");
+    // printf("22\n");
     
     if(q->count == 0) { // empty queue;
         q->front = new_node;
@@ -98,14 +103,14 @@ void QueuePush(int _qnum, Process *_p) {
     ++(q->count);
 }
 
-Process* QueueFront(int _qnum){
+int QueueFront(int _qnum){
     if(rq->process_queue[_qnum]->count == 0){
-        return NULL;
+        return -1;
     }
-    return rq->process_queue[_qnum]->front->p;
+    return rq->process_queue[_qnum]->front->q_pid;
 }
 
-void CPUCalc(){
+int CPUCalc(){
     int queue_pos = pid[processing_pid].queue_pos;
 
     // if(queue_pos == 3){
@@ -120,37 +125,53 @@ void CPUCalc(){
             //     pid[processing_pid].queue_pos += 1;
             // }
             pid[processing_pid].queue_pos += 1;
-            QueuePush(pid[processing_pid].queue_pos, &pid[processing_pid]);
+            if(pid[processing_pid].queue_pos == 3){
+                printf("3333\n");
+            }
+            QueuePush(pid[processing_pid].queue_pos, processing_pid);
         }
-        else{ // CPU타임이 끝나서 Asleep 상태로.
+        else{ // CPU타임이 끝나서 Asleep 상태로. //io가 0이면 asleep 보낼 필요 없다.
             for(int i=0; i<p_num; i++){
                 if(asleep_pid[i] == -1){
                     asleep_pid[i] = processing_pid;
+                    if(pid[processing_pid].io_burst_arr[pid[processing_pid].cur_cycle] != 0){
+                        pid[processing_pid].io_burst_arr[pid[processing_pid].cur_cycle] -= 1;
+                    }
                     break;
                 }
             }
         }
+        processing_pid = -1;
     }
     else if(pid[processing_pid].cpu_burst_arr[pid[processing_pid].cur_cycle] == 0){ // 현재 큐의 타임퀀텀이 남았는데 cpu burst가 끝나서 I/O asleep 상태 돌입. Q3 포함.
         pid[processing_pid].runtime = 0;
         for(int i=0; i<p_num; i++){
             if(asleep_pid[i] == -1){
                 asleep_pid[i] = processing_pid;
+                if(pid[processing_pid].io_burst_arr[pid[processing_pid].cur_cycle] != 0){
+                    pid[processing_pid].io_burst_arr[pid[processing_pid].cur_cycle] -= 1;
+                }
+                
+                processing_pid = -1;
                 break;
             }
         }
     }
+    else{ //there is a running process
+        return 1;
+    }
+    return -1;
 }
 
 void IOCalc(){
     for(int i=0; i<p_num; i++){
         if(asleep_pid[i] != -1){
             int pid_asleep = asleep_pid[i];
-            int cur_cycle = pid[pid_asleep].cur_cycle;
-            if(pid[pid_asleep].io_burst_arr[cur_cycle] == 0){ // I/O done
+            int cycle = pid[pid_asleep].cur_cycle;
+            if(pid[pid_asleep].io_burst_arr[cycle] == 0){ // I/O done
                 asleep_pid[i] = -1;
 
-                if(cur_cycle == pid[pid_asleep].cycles - 1){ // Process done
+                if(cycle == pid[pid_asleep].cycles - 1){ // Process done
                     pid[pid_asleep].process_end_time = timer;
                     continue;
                 }
@@ -158,60 +179,111 @@ void IOCalc(){
                     if(pid[pid_asleep].queue_pos != 0){
                         pid[pid_asleep].queue_pos -= 1;
                     }
-                    QueuePush(pid[pid_asleep].queue_pos, &pid[pid_asleep]);
+                    QueuePush(pid[pid_asleep].queue_pos, pid_asleep);
                     pid[pid_asleep].cur_cycle += 1;
                 }
             }
             
-            pid[pid_asleep].io_burst_arr[cur_cycle] -= 1;
+            pid[pid_asleep].io_burst_arr[cycle] -= 1;
         }
     }
 }
 
 int NewProcess(){
-    int flag = 0; // 그대로 0이면 모든 프로세스가 끝남.
+    int flag = 0; // 그대로 0이면 추가프로세스 없음
     for(int i=0; i<4; i++){
         if(rq->process_queue[i]->count != 0) {
-            Process* link = rq->process_queue[i]->front->p;
-            processing_pid = link->pid;
+            // printf("here 1\n");
+            processing_pid = QueueFront(i);//rq->process_queue[i]->front->q_pid; // QueueFront(i);
+            // processing_pid = link->pid;
+            // printf("here 2\n");
             QueuePop(i);
-            if(link->process_start_time == -1){
-                link->process_start_time = timer;
+            // printf("here 3\n");
+            if(pid[processing_pid].process_start_time == -1){
+                pid[processing_pid].process_start_time = timer;
             }
             flag = 1;
             break;
         }
     }
     if(flag == 0){
-        return 0;
+        return -1;
     }
     return 1;
+}
+void InAsleep(){
+    for(int i=0; i<p_num; i++){
+        printf("%d\t", asleep_pid[i]);
+    }
+    printf("\n");
+}
+void RQPrint(){
+    printf("TIME: %d\t", timer);
+    printf("RUNNING: %d\n", processing_pid);
+    for(int i=0; i<4; i++){
+        printf("QUEUE %d: ", i);
+        for(int j=0; j<rq->process_queue[i]->count; j++){
+            QNode *prn = rq->process_queue[i]->front;
+            printf("%d\t", prn->q_pid);
+            prn = prn->next;
+        }
+        printf("\n");
+    }
+    InAsleep();
+}
+
+int IOCheck(){
+    for(int i=0; i<p_num; i++){
+        if(asleep_pid[i] != -1){
+            return 1;
+        }
+    }
+    return -1;
 }
 
 int TimeTicker(){
     //AT 처리
+    RQPrint();
     for(int i=0; i<p_num; i++){
         if(pid[i].arrival_time == timer){
-            QueuePush(pid[i].queue_pos , &pid[i]);
+           // printf("here\n");
+            QueuePush(pid[i].queue_pos , i);
         }
+    } //printf("1\n");
+   
+    //현재 processing중인거 확인 후 처리. 근데 FCFS Q3는???
+    IOCalc();  //printf("2\n"); 
+    if(processing_pid != -1){ // for init.
+        running = CPUCalc(); 
     }
     
-    //처리하고나면 asleep에서 제외
-    IOCalc();
-    //현재 processing중인거 확인 후 처리. 근데 FCFS Q3는???
-    CPUCalc();
+    // printf("3\n");
+     //처리하고나면 asleep에서 제외
+    
     
     
     //RQ에서 processing할거 찾아서 넣기 (큐에 넣을거 다 넣은 이후) processing pid 교체.
     // 처음 프로세스 들어갈때 start time 없으면(-1) 기록하기
-    if(NewProcess() == 0){
-        return 0;
+    if(running == -1){
+        running = NewProcess();
     }
 
-    pid[processing_pid].runtime++;
-    pid[processing_pid].cpu_burst_arr[pid[processing_pid].cur_cycle] -= 1;
+    if(running == 1){
+        pid[processing_pid].runtime++;
+        // printf("%d\n", processing_pid);
+        // printf("%d\n", pid[processing_pid].cur_cycle);
+        pid[processing_pid].cpu_burst_arr[pid[processing_pid].cur_cycle] -= 1;
+    }
+    
+
+    
     // processing_time++;
     timer++;
+    
+    if(running == -1 && IOCheck() == -1){
+        return 0;
+    }
+    // printf("4\n");
     return 1;
 }
 
@@ -221,7 +293,8 @@ int main(int argc, char *argv[]) {
     ReadyQueueInit(rq);
     timer = 0;
     processing_time = 0;
-
+    processing_pid = -1;
+    running = -1;
     fscanf(input, "%d", &p_num);
     fgetc(input);
     pid = (Process*)malloc(sizeof(Process) * p_num);
@@ -233,6 +306,7 @@ int main(int argc, char *argv[]) {
     for(int i=0; i<p_num; i++){
         int _cycle;
         fscanf(input, "%d %d %d %d ", &pid[i].pid, &pid[i].arrival_time, &pid[i].queue_pos, &_cycle);
+        pid[i].pid -= 1;
         pid[i].cycles = _cycle;
         pid[i].cur_cycle = 0;
         pid[i].runtime = 0;
@@ -261,7 +335,7 @@ int main(int argc, char *argv[]) {
     }
     
     fclose(input);
-    while(TimeTicker()) {printf("111\n");}
+    while(TimeTicker()) {printf("\n");}
 
     ReadyQueueFree();
     free(rq);
