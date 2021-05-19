76d2b7488511da141763c8b39e5fc9a26e3df2ec
HAKSAN <cranemont@gmail.com>
2021-05-20 02:16:34 +0900

    0520

--- pa3/main.c
+++ pa3/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct heapNode{
+    int timeStamp;
+    int lastReferenceTime;
+    int referenceCount;
+    char pageNum;
+} heapNode;
+
+int time;
+
+void HeapInsertFIFO();
+void HeapInsertLRU();
+void HeapInsertLFU();
+
+void HeapPop();
+void HeapTop();
+
+
 int main(int argc, char** argv){
-    
+    time = 0;
+
 }
